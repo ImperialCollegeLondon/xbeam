d9313be22ebe08ec5b6233b0b784275b1e4153a1
fonsocarre <fonsocarre@gmail.com>
2017-03-31 13:55:34 +0100

    small changes in run_make for easier compilation and compatibility with sharpy

--- run_make.sh
+++ run_make.sh
@@ -1,15 +1,22 @@
 #! /bin/sh
+
+# This flag forces gfortran to print exactly when the
+# print statement is, but has some computational overhead.
+# Use only for debugging the library
 # export GFORTRAN_UNBUFFERED_ALL=1
+
 cd src/xbeam_base/
+mkdir -p obj
 cd obj
 rm *
 cd ..
 make
 cd ..
+mkdir -p obj
 cd obj
 rm *
 cd ..
 make
 cd ..
 make
-cp lib/* ../SHARPy/lib/
+cp lib/* ../sharpy/lib/
